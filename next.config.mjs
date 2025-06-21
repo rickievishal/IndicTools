41a6a7d75a31c929e5a71d393776f4e51bc7c3ae
Vishal Thangamaiappan <vishalthangamaiappan@Vishals-MacBook-Air.local>
2025-06-21 11:08:05 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
+const isGithub = process.env.GITHUB_PAGES === 'true'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
   trailingSlash: true, // required for GitHub Pages
   images: { unoptimized: true },
-  basePath: '/IndicTools',
+  basePath: isGithub ? '/IndicTools' : '',
 };
 
 export default nextConfig;
